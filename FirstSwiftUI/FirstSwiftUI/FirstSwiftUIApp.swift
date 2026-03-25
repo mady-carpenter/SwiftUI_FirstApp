d44b7a4ebe602c7ec0ef3dfc713eff726ec55c80
mady-carpenter <madycarpenter@gmail.com>
2026-03-25 12:49:10 -0500

    Add files via upload

---
+++ FirstSwiftUI/FirstSwiftUI/FirstSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  FirstSwiftUIApp.swift
+//  FirstSwiftUI
+//
+//  Created by Carpenter, Madeline F on 3/23/26.
+//
+
+import SwiftUI
+
+@main
+struct FirstSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
